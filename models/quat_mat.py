46b94b6446cd57e90790476dbf801372ad503d24
chen <37500268+chenxygh@users.noreply.github.com>
2022-03-30 20:14:40 +0800

    use pytorch3d: quaternion matrix  mutual conversion

--- models/quat_mat.py
+++ models/quat_mat.py
@@ -1,4 +1,8 @@
+import torch
 from scipy.spatial.transform import Rotation as R
+from pytorch3d.transforms import matrix_to_quaternion
+from pytorch3d.transforms import quaternion_to_matrix
+
 def Mat_to_Quat(matrix):
      '''
      matrix: input a 3D rotaion matrix
@@ -18,15 +22,19 @@ def Quat_to_Mat(quat):
 # TODO: dual quaternion and SE(3) matrix mutual conversion
 
 if __name__ == '__main__':
-     matrix = [[-0.89565879, 0.34191775, 0.28440742],
-               [ 0.08167378, 0.75506646, -0.65054134],
-               [-0.43717814, -0.55943445, -0.70420762]]
-     quat = [0.11563129, 0.91582379, -0.33029711, 0.19697718]
-     r = R.from_matrix(matrix)
-     q = r.as_quat()
-     print(q)
-     Q = R.from_quat(quat)
-     print(Q)
-     print(Q.as_matrix())
-     print(Quat_to_Mat(quat))
-     print(Mat_to_Quat(matrix))
+     matrices = [
+          [[-0.89565879, 0.34191775, 0.28440742],
+           [0.08167378, 0.75506646, -0.65054134],
+           [-0.43717814, -0.55943445, -0.70420762]],
+          [[0.7789, 0.5514, -0.2988],
+           [-0.5807, 0.4542, -0.6756],
+           [-0.2368, 0.6998, 0.6740]]
+     ]
+     quats = [0.11563129, 0.91582379, -0.33029711, 0.19697718]
+
+     ts_mat = torch.tensor(matrices)
+     print(ts_mat.shape)
+     res = matrix_to_quaternion(ts_mat)
+     print(res)
+     res = quaternion_to_matrix(res)
+     print(res)
